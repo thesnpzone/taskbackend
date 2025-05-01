5957a9ff26295d9c5322096b5ed5db77899630ee
snpzone <65709045+snpzone@users.noreply.github.com>
2025-05-01 23:37:56 +0530

    update the cros

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const PORT = process.env.PORT;
 
 app.use(
     cors({
-        origin: ["http://localhost:5173", "http://127.0.0.1:5500"],
+        origin: ["http://localhost:5173", "http://127.0.0.1:5500", "https://cluematrixtask.netlify.app"],
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
         credentials: true,
     })
@@ -50,7 +50,7 @@ app.use(express.urlencoded({ extended: true, limit: "150mb" }));
 // ======================
 
 connectDB();
-//c
+
 // ============================================ //
 // ********** Routes  section Start ********** //
 // =========================================== //
