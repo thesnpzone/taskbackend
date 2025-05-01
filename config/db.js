d8e2819904d97ac43af527a78a964ba1a95eb098
snpzone <65709045+snpzone@users.noreply.github.com>
2025-05-01 22:42:11 +0530

    db update

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async() => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/student_auth');
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("✅ MongoDB Connected");
     } catch (err) {
         console.error("❌ MongoDB connection error:", err);
-        process.exit(1); // Exit the process with failure
+        process.exit(1);
     }
 };
 
